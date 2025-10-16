42c3497a4ef2c22e1af5dbe294fa184bd0bd5bae
rafaelscheidtt <rafaelscheidt@outlook.com>
2025-10-16 16:42:46 -0300

    feat: Implementação funcionalidades dos botões da tela inicial

--- frontend/javascript/app.js
+++ frontend/javascript/app.js
@@ -1,4 +1,38 @@
+// PARA O MENU HAMBÚRGUER ---
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+
+// ROLAGEM SUAVE (UNIVERSAL)
+document.addEventListener('DOMContentLoaded', () => {
+
+    
+    const internalLinks = document.querySelectorAll('a[href^="#"]');
+
+    internalLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href');
+
+            // verificação para não dar erro em links com apenas "#"
+            if (targetId.length > 1) {
+                const targetSection = document.querySelector(targetId);
+
+                if (targetSection) {
+                    targetSection.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    });
+
+                    
+                    if (nav.classList.contains('active')) {
+                        nav.classList.remove('active');
+                    }
+                }
+            }
+        });
+    });
+});
