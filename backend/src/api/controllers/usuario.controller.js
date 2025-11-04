42e462129c08b28c8f9c2d4a3aabce2976e8c62c
Matheus Ming <minggiongo11@gmail.com>
2025-11-04 02:25:36 -0300

    Implementação da funcionalidade de getUsuarioById

--- backend/src/api/controllers/usuario.controller.js
+++ backend/src/api/controllers/usuario.controller.js
@@ -1,3 +1,53 @@
+const mongoose = require("mongoose")
+
 const Usuario = require("../../models/Usuario.model")
 
+const {
+  ERRO,
+  USUARIO
+} = require("../../constants/responseMessages.constants")
 
+exports.getUsuarioById = async (req, res) => {
+  try
+  {
+    const {id} = req.params
+
+    if(!id)
+    {
+      return res.status(400).json({message: USUARIO.ID_NAO_FORNECIDO})
+    }
+
+    if(!mongoose.Types.ObjectId.isValid(id))
+    {
+      return res.status(400).json({message: USUARIO.ID_FORNECIDO_INVALIDO})
+    }
+
+    const usuario = await Usuario.findById(id).select("-__v")
+
+    if(!usuario)
+    {
+      return res.status(400).json({message: USUARIO.USUARIO_NAO_ENCONTRADO})
+    }
+
+    res.status(200).json({
+      message: USUARIO.USUARIO_ENCONTRADO,
+      user: usuario
+    })
+  }
+  catch(error)
+  {
+    return res.status(500).json({message: ERRO.ERRO_INTERNO_NO_SERVIDOR}) // código 500, internal server error
+  }
+}
+
+exports.getTodosUsuarios = async (req, res) => {
+
+}
+
+exports.updateUsuarioById = async (req, res) => {
+
+}
+
+exports.deleteUsuarioById = async (req, res) => {
+
+}
--- backend/src/api/routes/usuario.routes.js
+++ backend/src/api/routes/usuario.routes.js
@@ -5,10 +5,20 @@ const usuarioController = require("../controllers/usuario.controller")
 const authMiddleware = require("../../middlewares/auth.middleware")
 const checkRole = require("../../middlewares/checkRole.middleware")
 
+const {
+  PERMISSAO_PARA_VER_OS_DADOS_DOS_USUARIOS
+} = require("../../constants/permission.constants")
+
 // Rotas públicas
 
 // Rotas protegidas
 
 // Rotas proibidas
+router.get(
+  "/:id",
+  authMiddleware,
+  checkRole(PERMISSAO_PARA_VER_OS_DADOS_DOS_USUARIOS),
+  usuarioController.getUsuarioById
+)
 
 module.exports = router
